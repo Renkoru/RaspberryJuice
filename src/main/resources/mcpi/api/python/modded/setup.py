ce2f7761373b74ca9574ad1536a85359540bf461
Maxim Rurenko <mrurenko@lineate.com>
2018-02-05 00:48:12 +0600

    add setup file for python package mcpi

---
+++ src/main/resources/mcpi/api/python/modded/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='mcpi',
+      version='0.1',
+      description='Python sdk package to talk to RaspberryJuice backend',
+      url='https://github.com/Renkoru/RaspberryJuice',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
